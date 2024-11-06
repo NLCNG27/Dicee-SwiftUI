9e662e0eebbb9d6dc6e5659d4a273489f442ae2b
Nguyen Lam <nguyen@cngsoftware.com>
2024-11-06 14:58:24 -0600

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Nguyen Lam on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
